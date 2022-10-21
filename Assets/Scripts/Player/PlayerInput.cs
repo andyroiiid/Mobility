ad9eb42ac0b89e252862a87df1c7f77dd29388b0
Andrew Huang <andyroiiid@live.com>
2022-10-21 13:47:21 -0500

    Input fix

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -1,14 +1,19 @@
-using Player.Input;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 namespace Player
 {
     public class PlayerInput : MonoBehaviour
     {
-        [SerializeField] private InputActionVector2 moveInput;
-        [SerializeField] private InputActionVector2 lookInput;
-        [SerializeField] private InputActionButton jumpInput;
+        [SerializeField]
+        private InputAction moveInput;
+
+        [SerializeField]
+        private InputAction lookInput;
+
+        [SerializeField]
+        private InputAction jumpInput;
 
         public UnityAction<Vector2> OnMove;
         public UnityAction<Vector2> OnLook;
@@ -19,9 +24,33 @@ namespace Player
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
 
-            moveInput.Enable(OnMove);
-            lookInput.Enable(OnLook);
-            jumpInput.Enable(OnJump, null);
+            moveInput.Enable();
+            moveInput.started += OnMoveInput;
+            moveInput.performed += OnMoveInput;
+            moveInput.canceled += OnMoveInput;
+
+            lookInput.Enable();
+            lookInput.started += OnLookInput;
+            lookInput.performed += OnLookInput;
+            lookInput.canceled += OnLookInput;
+
+            jumpInput.Enable();
+            jumpInput.started += OnJumpInput;
+        }
+
+        private void OnMoveInput(InputAction.CallbackContext ctx)
+        {
+            OnMove?.Invoke(ctx.ReadValue<Vector2>());
+        }
+
+        private void OnLookInput(InputAction.CallbackContext ctx)
+        {
+            OnLook?.Invoke(ctx.ReadValue<Vector2>());
+        }
+
+        private void OnJumpInput(InputAction.CallbackContext ctx)
+        {
+            OnJump?.Invoke();
         }
     }
 }
